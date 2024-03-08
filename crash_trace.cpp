21b0690d50fe756c80a4e903061c714c96fcf365
wz <1208827324@qq.com>
2024-03-08 21:19:28 +0800

    修改Crash打印

---
+++ crash_trace.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <cxxabi.h> // abi::__cxa_demangle
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // stderror
+#include <signal.h> // signal
+#include <execinfo.h> // backtrace, backtrace_symbols, backtrace_symbols_fd
+#include <dlfcn.h> // dladdr
+#include <link.h> // _r_debug.r_map->l_addr
+
+#include "crash_trace.h"
+
+void crash_trace_dump(int signum)
+{
+    void *bt_buffer[256];
+	size_t size = backtrace(bt_buffer, 256);
+
+	// Dump the backtrace to stderr with a message to the user
+	printf("\n================================================================\n");
+	printf("%s: Program crashed with signal %d\n", __FUNCTION__, signum);
+
+	char **strings = backtrace_symbols(bt_buffer, size);
+	// PIE executable relocation, zero for non-PIE executables
+#ifdef __GLIBC__
+	// This is a glibc only thing apparently.
+	uintptr_t relocation = _r_debug.r_map->l_addr;
+#else
+	// Non glibc systems apparently don't give PIE relocation info.
+	uintptr_t relocation = 0;
+#endif //__GLIBC__
+	if (strings) 
+    {
+		for (size_t i = 1; i < size; i++) 
+        {
+			char fname[1024];
+			Dl_info info;
+
+			snprintf(fname, 1024, "%s", strings[i]);
+
+			// Try to demangle the function name to provide a more readable one
+			if (dladdr(bt_buffer[i], &info) && info.dli_sname) 
+            {
+				if (info.dli_sname[0] == '_') 
+                {
+					int status = 0;
+					char *demangled = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+
+					if (status == 0 && demangled) 
+                    {
+						snprintf(fname, 1024, "%s", demangled);
+					}
+					if (demangled) 
+                    {
+						free(demangled);
+					}
+				}
+			}
+
+			printf("[%lu] %s\n", i, fname);
+		}
+
+		free(strings);
+	}
+	printf("-- END OF BACKTRACE --\n");
+	printf("================================================================\n");
+}
+
+static void signal_handler(int signum)
+{
+    crash_trace_dump(signum);
+
+    signal(signum, SIG_DFL); // recover signal
+    raise(signum);
+}
+
+void crash_trace_init(void)
+{
+#if CRASH_TRACE_ENABLE
+    signal(SIGSEGV, signal_handler);
+    signal(SIGABRT, signal_handler);
+#endif
+}
+
+void crash_trace_deinit(void)
+{
+#if CRASH_TRACE_ENABLE
+    signal(SIGSEGV, SIG_DFL); // recover signal
+    signal(SIGABRT, SIG_DFL); // recover signal
+#endif
+}
--- crash_trace.h
+++ crash_trace.h
@@ -1,19 +1,12 @@
-#ifndef __BACKTRACE_H__
-#define __BACKTRACE_H__
+#ifndef __CRASH_TRACE_H__
+#define __CRASH_TRACE_H__
 
 #include <stdio.h>
 
 // crash trace enable
 #define CRASH_TRACE_ENABLE   1
 
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-
 void crash_trace_init(void);
 void crash_trace_deinit(void);
 
-#ifdef __cplusplus
-}
-#endif // __cplusplus
 #endif // !__BACKTRACE_H__
